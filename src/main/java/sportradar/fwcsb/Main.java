50c7424d3c87bf7e72e739b6ad4eef3f82743076
bartoszjanuszkiewicz <bartosz.januszkiewicz_ext@autoscout24.com>
2025-07-16 20:53:23 +0200

    Remove not needed classes

--- src/main/java/sportradar/fwcsb/Main.java
+++
@@ -1,8 +0,0 @@
-package sportradar.fwcsb;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Football World Cup Score Board app");
-    }
-}
